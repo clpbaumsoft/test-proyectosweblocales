00c557c8f276e2238969481a4e63a12eb26f0c1e
Kevin Daza <kevin.daza@baumsoft.co>
2025-12-09 14:54:28 -0500

    Commit to save progress - Change the font size of login button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -12,7 +12,7 @@ export default function Button({ text, type, icon, onClick, disabled }: ButtonPr
         bg-proquinal-teal 
         px-3.5 
         py-2.5 
-        text-sm 
+        text-[12px] 
         font-semibold 
         text-white 
         shadow-sm 
@@ -21,11 +21,7 @@ export default function Button({ text, type, icon, onClick, disabled }: ButtonPr
         focus-visible:outline 
         focus-visible:outline-2 
         focus-visible:outline-offset-2 
-        focus-visible:outline-indigo-600 
-        dark:bg-indigo-500 
         dark:shadow-none 
-        dark:hover:bg-indigo-400 
-        dark:focus-visible:outline-indigo-500
       "
     >
       {icon && icon}
